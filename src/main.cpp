c626b0f31942f251f558ab25452006718c1539d3
Alexander Paschold <alexander.paschold@thinprint.de>
2021-12-30 10:46:15 +0100

    send MQTT message

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 #include <ESP8266HTTPClient.h>
 #include <tr064.h>
+#include <PubSubClient.h>
 
 #define LED_BUILTIN 2
 
@@ -16,12 +17,78 @@ const char PASSWORD[] = "doorbell.12683.82B";
 const char FRITZBOX_IP[] = "192.168.178.1";
 const int FRITZBOX_PORT = 49000;
 
+const char* mqtt_broker = "192.168.178.55";
+const char* topic_doorbell = "doorbell/ring";
+
+unsigned long ulStart = 0;
+
 WiFiClient wifi_client;
+PubSubClient mqtt_client(wifi_client);
+
 
 TR064 tr064_connection(FRITZBOX_PORT, FRITZBOX_IP, USER, PASSWORD);
 
+void reconnect_mqtt()
+{
+    while (!mqtt_client.connected())
+    {
+        Serial.print("Reconnecting MQTT ...\r\n");
+        if (!mqtt_client.connect("ESP8266Client"))
+        {
+            Serial.print("failed, rc=\r\n");
+            Serial.print(mqtt_client.state());
+            Serial.println("retrying in 100 milliseconds\r\n");
+            delay(100);
+        }
+    }
+}
+
+void ringAllPhones()
+{
+  Serial.println("TR-064 - try round call\r\n");
+
+  /*Serial.println("TR-064 - init ...");
+  tr064_connection.init();
+
+  Serial.println(" done\r\n");*/
+
+  String tr064_service = "urn:dslforum-org:service:X_VoIP:1";
+  String call_params[][2] = {{"NewX_AVM-DE_PhoneNumber", "**9"}}; //Die Telefonnummer **9 ist der Fritzbox-Rundruf.
+
+  //if (tr064_connection.action(tr064_service, "X_AVM-DE_DialNumber", call_params, 1) == true)
+  if (tr064_connection.action(tr064_service, "X_AVM-DE_DialNumber", call_params, 1, "/upnp/control/x_voip") == true)
+  {
+    Serial.println("TR-064 action successfull\r\n");
+  }
+  else
+  {
+    Serial.println("TR-064 action failed\r\n");
+  }
+
+  //Warte vier Sekunden bis zum auflegen
+  delay(4000);
+
+  tr064_connection.action(tr064_service, "X_AVM-DE_DialHangup");
+}
+
+void sendMqtt(const char* const payload)
+{
+  if (!mqtt_client.connected())
+    reconnect_mqtt();
+
+  Serial.printf("sending '%s'\r\n", payload);
+  mqtt_client.publish(topic_doorbell, "ring");
+
+  mqtt_client.loop();
+
+  unsigned long ulTime = millis() - ulStart;
+  Serial.printf("time from reset: %lu ms\r\n", ulTime);
+}
+
 void setup() 
 {
+  ulStart = millis();
+
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(9600);
@@ -65,6 +132,8 @@ void setup()
   Serial.println(WiFi.localIP());
   Serial.println("\r\n");
 
+  mqtt_client.setServer(mqtt_broker, 1883);
+
   tr064_connection.debug_level = TR064::DEBUG_VERBOSE;
 
   //ESP.wdtDisable();
@@ -72,29 +141,9 @@ void setup()
 
 void loop()
 {
+  //ringAllPhones();
 
-  Serial.println("TR-064 - try round call\r\n");
+  sendMqtt("ring");
 
-  /*Serial.println("TR-064 - init ...");
-  tr064_connection.init();
-
-  Serial.println(" done\r\n");*/
-
-  String tr064_service = "urn:dslforum-org:service:X_VoIP:1";
-  String call_params[][2] = {{"NewX_AVM-DE_PhoneNumber", "**9"}}; //Die Telefonnummer **9 ist der Fritzbox-Rundruf.
-
-  //if (tr064_connection.action(tr064_service, "X_AVM-DE_DialNumber", call_params, 1) == true)
-  if (tr064_connection.action(tr064_service, "X_AVM-DE_DialNumber", call_params, 1, "/upnp/control/x_voip") == true)
-  {
-    Serial.println("TR-064 action successfull\r\n");
-  }
-  else
-  {
-    Serial.println("TR-064 action failed\r\n");
-  }
-
-  //Warte vier Sekunden bis zum auflegen
-  delay(4000);
-
-  tr064_connection.action(tr064_service, "X_AVM-DE_DialHangup");
+  ESP.deepSleep(0); 
 }
